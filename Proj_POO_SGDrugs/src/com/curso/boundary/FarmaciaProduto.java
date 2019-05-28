a46a789873230ecc8c94071f2f69a8a1b0e31f74
RaphaelMendes203 <raphael.victor204@gmail.com>
2019-05-28 15:19:48 -0300

    Criação do boundary FarmaciaProduto

---
+++ Proj_POO_SGDrugs/src/com/curso/boundary/FarmaciaProduto.java
@@ -0,0 +1,229 @@
+package com.curso.boundary;
+
+import java.io.FileInputStream;
+
+import javax.xml.ws.handler.Handler;
+import javax.xml.ws.handler.MessageContext;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class FarmaciaProduto extends Application implements EventHandler<MouseEvent>{
+
+	private Button btnCadProd;
+	private Button btnMantProd, btnCadastro, btnPesquisaProd;
+	private BorderPane painelCad;
+	private BorderPane painelMant;
+	private TextField txtQntd;
+	private TextField txtPrecoUnit;
+	private TextField txtPesquisaProd;
+	private ComboBox<String> cmbGrupo;
+	private ComboBox<String> cmbSessao;
+	private ComboBox<String> cmbTipoPesquisa;
+	private HBox menuTop;
+	
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+//INICIO FORMULARIO CADASTRO PRODUTO FARMACIA---------------------------------------
+
+		txtQntd = new TextField();
+		txtQntd.setPromptText("Qntd");
+		txtPrecoUnit = new TextField();
+		txtPrecoUnit.setPromptText("Preço unitario");
+		cmbGrupo = new ComboBox<String>();
+		cmbGrupo.setPromptText("Grupo");
+		cmbSessao = new ComboBox<String>();
+		cmbSessao.setPromptText("Sessão");
+		btnCadastro = new Button("CADASTRAR");
+		ImageView iv = new ImageView(new Image(new FileInputStream("imgs\\icon.png")));
+		iv.setFitHeight(22);
+		iv.setFitWidth(22);
+		btnPesquisaProd = new Button("", iv);
+		txtPesquisaProd = new TextField();
+		cmbTipoPesquisa = new ComboBox<String>();
+		cmbTipoPesquisa.setPromptText("Tipo");
+		
+		HBox hb = new HBox(80,
+				new VBox(10,
+						new Label("Produto selecionado: "),
+						new Label("Fornecedor: "),
+						new HBox(10, txtQntd, txtPrecoUnit),
+						new HBox(10, cmbGrupo, cmbSessao),
+						btnCadastro
+				),
+				new VBox(10,
+						new Label("Produtos cadastrados"),
+						new Separator(),
+						new HBox(10, cmbTipoPesquisa, new HBox(txtPesquisaProd, btnPesquisaProd)),
+						new TableView<>()
+				)
+		);
+		hb.setStyle("-fx-font-size: 15px;");
+		
+//FIM FORMULARIO CADASTRO PRODUTO FARMACIA------------------------------------------		
+		
+		BorderPane pane = new BorderPane();
+		
+		btnCadProd = new Button("CADASTRO");
+		btnMantProd = new Button("GERENCIAMENTO");
+		painelCad = new BorderPane(hb);
+		painelMant = new BorderPane();
+		
+		menuTop = new HBox(btnCadProd, btnMantProd);
+		pane.setTop(menuTop);
+		StackPane painels = new StackPane(painelMant, painelCad);
+		pane.setCenter(painels);
+		
+		Scene scene = new Scene(pane, 1100,600);
+		stage.setScene(scene);
+		stage.setTitle("Manter Clientes");
+		stage.show();
+		
+		startStyle();
+		btnSelected(0);
+		
+		btnCadProd.addEventHandler(MouseEvent.MOUSE_CLICKED, this);
+		btnMantProd.addEventHandler(MouseEvent.MOUSE_CLICKED, this);
+		
+	}
+	
+public void startStyle() {
+		
+		DropShadow dp = new DropShadow(4, 0, 0, Color.GRAY);
+		
+		String styleBtnPesquisa = 
+				"-fx-background-color: #0095FE;"
+				+ "-fx-text-fill: white;"
+				+ "-fx-background-radius: 7;"
+				+ "-fx-min-width: 240px;"
+				+ "-fx-min-height: 30px;"
+				+ "-fx-cursor: hand;";
+		
+		String styleBtns = 
+				"-fx-background-color: #0095FE;"
+				+ "-fx-text-fill: white;"
+				+ "-fx-background-radius: 7;"
+				+ "-fx-min-width: 275px;"
+				+ "-fx-min-height: 40px;"
+				+ "-fx-cursor: hand;";
+		
+		String styleBtnAddProb =
+				"-fx-min-width: 625px;"
+				+ "-fx-background-color: #007F0E;"
+				+ "-fx-text-fill: white;"
+				+ "-fx-font-size: 15px;"
+				+ "-fx-background-radius: 8;"
+				+ "-fx-cursor: hand;";
+		
+		String styleBtn =
+				"-fx-background-radius: none;"
+				+ "-fx-min-width: 130px;"
+				+ "-fx-min-height: 40px;"
+				+ "-fx-cursor: hand;"
+				+ "-fx-font-color: #545452;"
+				+ "-fx-font-weight: bold;";
+		
+		String stylePainel = 
+				"-fx-background-color: #FEFFFA;"
+				+ "-fx-padding: 50, 50, 50, 50;";
+		
+		String styleMenuTop = "-fx-background-color: #E0DACE";
+		
+		String styleEntradaDataNasc = "-fx-min-width: 130px;";
+		
+		String styleEntradas = "-fx-background-radius: 8;";
+		
+		String styleEntradaPesquisa = "-fx-background-radius: 8px 0px 0px 8px;"
+				+ "-fx-min-width: 537px;";
+		
+		String stylePesquisaProb = "-fx-min-height: 30px;"
+				+ "-fx-min-width: 30px;"
+				+ "-fx-background-radius: 0px 8px 8px 0px;"
+				+ "-fx-background-color: #0095FE;"
+				+ "-fx-cursor: hand;";
+		
+		String comboStyle = "-fx-background-radius: 8;"
+				+ "-fx-background-color: #FEFFFA;"
+				+ "-fx-cursor: hand;";
+		
+		btnCadProd.setStyle(styleBtn);
+		btnMantProd.setStyle(styleBtn);
+		painelCad.setStyle(stylePainel);
+		painelMant.setStyle(stylePainel);
+		menuTop.setStyle(styleMenuTop);
+		txtPesquisaProd.setStyle(styleEntradaPesquisa + "-fx-font-size: 15.1px");
+		btnPesquisaProd.setStyle(stylePesquisaProb);
+		cmbTipoPesquisa.setStyle(comboStyle + "-fx-font-size: 15.1px");
+		cmbTipoPesquisa.setEffect(dp);
+		cmbGrupo.setStyle(comboStyle + "-fx-font-size: 20px; -fx-min-width: 248px;");
+		cmbGrupo.setEffect(dp);
+		cmbSessao.setStyle(comboStyle + "-fx-font-size: 20px; -fx-min-width: 248px;");
+		cmbSessao.setEffect(dp);
+		txtQntd.setStyle(styleEntradas + "-fx-font-size: 20px;");
+		txtPrecoUnit.setStyle(styleEntradas + "-fx-font-size: 20px;");
+		btnCadastro.setStyle(styleBtns + "-fx-font-size: 15px; -fx-min-width: 506;");
+
+	}
+	
+	public void btnSelected(int btn) {
+		String CadSelected = "";
+		String MantSelected = "";
+		if(btn == 0) {
+			CadSelected = "#FEFFFA";
+			MantSelected = "#EAEAEA";
+		}else {
+			CadSelected = "#EAEAEA";
+			MantSelected = "#FEFFFA";
+		}
+		btnCadProd.setStyle(
+				"-fx-background-color: " + CadSelected + ";"
+				+ "-fx-background-radius: none;"
+				+ "-fx-min-width: 130px;"
+				+ "-fx-min-height: 40px;"
+				+ "-fx-cursor: hand;"
+		);
+		btnMantProd.setStyle(
+				"-fx-background-color: " + MantSelected + ";"
+				+ "-fx-background-radius: none;"
+				+ "-fx-min-width: 130px;"
+				+ "-fx-min-height: 40px;"
+				+ "-fx-cursor: hand;"
+		);
+	}
+	
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+	@Override
+	public void handle(MouseEvent e) {
+		if(e.getSource() == btnCadProd) {
+			btnSelected(0);
+			painelCad.toFront();
+		}else
+		if(e.getSource() == btnMantProd) {
+			btnSelected(1);
+			painelMant.toFront();
+		}
+	}
+
+}
